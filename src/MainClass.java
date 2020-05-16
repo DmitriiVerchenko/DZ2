33bab53e7ae56ac7f61ee42863a4c5a59a33a782
Dmitrii <d3dax@mail.ru>
2020-05-16 22:54:01 +0500

    1

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MainClass {
     public static void main (String[] args){
-
+        // 1 задание
         int[] array = new int []{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0;i < array.length;i++) {
             if(0 != array[i]){
@@ -16,14 +16,14 @@ public class MainClass {
 
         }
         System.out.println(Arrays.toString(array));
-
+        // 2 задание
         int [] array1 = new int [8];
         for (int i = 0; i < array1.length; i++) {
         array1[i] = i * 3;
 
         }
        System.out.println(Arrays.toString(array1));
-
+        // 3 задание
         int[] array2 = new int []{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i <array2.length; i++) {
             if (array2[i] < 6){
@@ -33,13 +33,7 @@ public class MainClass {
         }
         System.out.println(Arrays.toString(array2));
 
-//        int [][] array3 = new int[][]{
-//                 {1, 2, 3, 4, 5},
-//                 { 6, 7, 8, 9, 10},
-//                 { 11, 12, 13, 14, 15},
-//                 { 16, 17, 18, 19, 20},
-//                 { 21, 22, 23, 24, 25}
- //       };
+        // 4 задание
         int [][] array3 = new int[5][5];
         for (int i = 0; i <array3.length; i++) {
             for (int j = 0, j2 = array3[i].length; j < array3[i].length; j++, j2--) {
@@ -49,7 +43,8 @@ public class MainClass {
             }
             System.out.println();
         }
-            int [] array4 = new int[]{10,2,3,4,5,6,7};
+        // 5 задание
+        int [] array4 = new int[]{10,2,3,4,5,6,7};
             int min = array4[0];
             int max = array4[0];
             for (int i = 0; i <array4.length; i++) {
